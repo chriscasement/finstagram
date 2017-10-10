eafbc690893f175084a7bcdcdf72f2975d37dcce
Chris <ccasement@icloud.com>
2017-10-10 01:48:41 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -93,5 +93,34 @@ end
 
 get '/posts/:id' do
     @post = Post.find(params[:id]) # find the post with the ID from the URL
-    escape_html @post.inspect      # print to the screen for now
+    erb(:"posts/show")              # render app/views/posts/show.erb
+end
+
+post '/comments' do
+    # point values from params to variables
+    text = params[:text]
+    post_id = params[:post_id]
+    
+    # instantiate a comment with those values & assign the comment to the 'current_user'
+    comment = Comment.new({ text: text, post_id: post_id, user_id: current_user.id })
+    
+    # save the comment
+    comment.save
+    
+    # 'redirect' back to wherever we came from
+    redirect(back)
+end
+post '/likes' do
+    post_id = params[:post_id]
+    
+    like = Like.new({ post_id: post_id, user_id: current_user.id })
+    like.save
+    
+    redirect(back)
+end
+
+delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,5 @@
 class Like < ActiveRecord::Base
     
+    validates_presence_of :user, :post
+    
 end
